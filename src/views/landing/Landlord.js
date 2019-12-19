c54c0ea0cd8c7badfa527511b662433d3d702615
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-12-18 20:39:16 -0500

    more problems

--- src/views/landing/Landlord.js
+++ src/views/landing/Landlord.js
@@ -1,28 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-import SubBlock from '../../components/Landing/SubBlock';
-import Header from '../../components/Landing/Header';
-
-const Landlord = () => {
-  return (
-    <div className="landlordView">
-      <div className="landlordBlock">
-        <div className="landlordHeader">
-          <Header />
-        </div>
-        <div className="landlordSubs">
-          <SubBlock />
-          <SubBlock />
-          <SubBlock />
-        </div>
-      </div>
-    </div>
-  );
-=======
 
 const Landlord = () => {
   return <div className="landlordView">Landlord</div>;
->>>>>>> f3b24845cfcdd069ba2cbaf0c888f014770ece0c
 };
 
 export default Landlord;
